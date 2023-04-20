41ac56dcc981d34e422ed77dc5acc878107122e4
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-04-21 00:28:55 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello dev");
+// import "regenerator-runtime/runtime";
+import axios from "axios";
+
+// import { API_URL, URL_COMICS } from "./constants/api";
+// import { getDataApi } from "./utils/getDataApi";
+
+// (async () => {
+//   const data = await getDataApi.getData(API_URL + URL_COMICS);
+//   console.log(data);
+// })();
+
+import { guitarCatalog } from "./catalog/guitar-catalog.json";
+
+const url =
+  "https://gist.githubusercontent.com/dev-pandaren/a3ad2d177d18402e609ec743676af809ec743676af80a/raw/3889c469f25c71ec11e628acdd12438742506504/guitar-catalog.json";
+
+axios
+  .get(guitarCatalog)
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error.message));
